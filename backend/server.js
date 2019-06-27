88772368d1d6535633c23c2ff9f67e2c4a793d50
TheKaizenWolf <raul670saavedra@gmail.com>
2019-06-27 18:53:18 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(3000, () =>  {
-	console.log('Server is working!');
+app.listen(process.env.PORT, () =>  {
+	console.log(`App is running on ${process.env.PORT}`);
 })
