0b21347d0bfd81533ffd10832c6f46b23c21fd40
TheKaizenWolf <raul670saavedra@gmail.com>
2019-06-27 08:02:13 -0400

    finishing changes

--- server/server.js
+++ server/server.js
@@ -23,43 +23,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-		users: [
-		{
-			id: '123',
-			name: 'hehe',
-			email: 'hehe@gmail.com',
-			password: 'water',
-			entries: 0,
-			joined: new Date(),
-		},
-		{
-			id: '124',
-			name: 'haha',
-			email: 'haha@gmail.com',
-			password: 'beef',
-			entries: 2,
-			joined: new Date()
-		}
-	]
-}
+
 
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.send(db.users);
 })
 
 app.post('/signin', (req, res) => {
-	bcrypt.compare('chicken', '$2b$10$uYpsRQyS2COlzT8GSrwAFOdGp3kI7QfFGrKuvKY1svKuJFn6LC/QO', function(err, res) {
-	  console.log('first guess', res);
-	});
-	bcrypt.compare('veggies', '$2b$10$uYpsRQyS2COlzT8GSrwAFOdGp3kI7QfFGrKuvKY1svKuJFn6LC/QO', function(err, res) {
-	  console.log('second guess', res);
-	});
-	if (req.body.email === database.users[1].email && req.body.password === database.users[1].password) {
-			res.json(database.users[1]);
-	} else {
-		res.status(400).json('error logging in');
-	}
+	db.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+				} else {
+					return res.status(400).json('wrong credentials')
+				}
+			})
+		.catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
@@ -72,18 +58,22 @@ app.post('/register', (req, res) => {
 		})
 		.into('login')
 		.returning('email')
-		.then(loginEmail => )
+		.then(loginEmail => {
+			return trx('users')
+			.returning('*')
+			.insert({
+				name: name,
+				email: loginEmail[0],
+				joined: new Date() 
+			})
+			.then(user => {
+				res.json(user[0]);
+			})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
 	})
-	return db('users')
-		.returning('*')
-		.insert({
-			name: name,
-			email: email,
-			joined: new Date() 
-		})
-		.then(user => {
-			res.json(user[0]);
-		})
+	
 	.catch(err => res.status(400).json('unable to register'))
 })
 
--- src/components/Rank.js
+++ src/components/Rank.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TextDiv = styled.div.attrs({
-	className: 'mt4 dim black f4 fw9 b db mb2'
+	className: 'mt4 dim white f3 fw9 b db mb2'
 })``
 const Rank = ({name, entries}) => {
 	return (
 		<div className ='flex justify-center'>
 			<TextDiv>
-				{`${name}, your current rank is ${entries}`} 
+				{`${name}, your current entry count is ${entries}`} 
 			</TextDiv>
 		</div>
 
