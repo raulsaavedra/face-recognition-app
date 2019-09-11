9d665e226ac730ac4c2bb93b49881a0598b72a70
TheKaizenWolf <raul670saavedra@gmail.com>
2019-09-11 14:22:35 -0300

    update

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ const app = express();
 app.use(cors());
 app.use(morgan('combined'))
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname+'../frontend/build/index.html'));
 });
 
 app.get('/api', (req, res) => {res.send('ITS WORKING!')})
