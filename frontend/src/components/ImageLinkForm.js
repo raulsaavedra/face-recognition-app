8f7f73233742ad2a80d27a0b6527a57109da9ccc
TheKaizenWolf <raul670saavedra@gmail.com>
2019-09-24 19:32:36 -0300

    update

--- frontend/src/components/ImageLinkForm.js
+++ frontend/src/components/ImageLinkForm.js
@@ -7,7 +7,7 @@ import {FaArrowDown} from 'react-icons/fa';
 const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 	return (
 		<FormDiv>
-		    <FaArrowDown className='animated swing infinite dim black f1 mb2'/>
+		    <FaArrowDown className='animated swing infinite dim white f1 mb2'/>
 		    <FormInnerDiv>
 			    <StyledInput 
 			    	id='name' 
@@ -39,14 +39,12 @@ const FormInnerDiv = styled.div.attrs({
   className: 'flex-l justify-center pa4 br4'
 })`
 		background:
-    radial-gradient(black 3px, transparent 4px),
-    radial-gradient(black 3px, transparent 4px),
-    linear-gradient(#fff 4px, transparent 0),
-    linear-gradient(45deg, transparent 74px, transparent 75px, #a4a4a4 75px, #a4a4a4 76px, transparent 77px, transparent 109px),
-    linear-gradient(-45deg, transparent 75px, transparent 76px, #a4a4a4 76px, #a4a4a4 77px, transparent 78px, transparent 109px),
-    #fff;
-    background-size: 109px 109px, 109px 109px,100% 6px, 109px 109px, 109px 109px;
-    background-position: 54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px;
+		radial-gradient(black 15%, transparent 16%) 0 0,
+		radial-gradient(black 15%, transparent 16%) 8px 8px,
+		radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+		radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+		background-color:#282828;
+		background-size:16px 16px;
 `
 const StyledInput = styled.input.attrs({
 	className: 'input-reset bg-white shadow-5 pa2 pa3-m w-100'
