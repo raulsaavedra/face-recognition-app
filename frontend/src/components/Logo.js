b362836bb16020d0322f34e64ecd1af707736485
TheKaizenWolf <raul670saavedra@gmail.com>
2019-09-24 19:05:49 -0300

    update

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -14,11 +14,14 @@ const StyledTilt = styled(Tilt).attrs({
 `
 const Logo = ({showResults}) => {
 	return (
+			<div> { this.state.showResults ? null : (
 			<StyledLogo>
-				<StyledTilt className={showResults ? 'dn' : ''} options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+				<StyledTilt options={{ max : 25 }} style={{ height: 250, width: 250 }} >
 				 <div className="Tilt-inner"><img alt='' src={logo}/></div>
 				</StyledTilt>
 			</StyledLogo>
+			)}
+			</div>
 		)
 }
 
